c3c7089e587fcbcd696de651fb014f50bac300fa
ZED-Magdy <zedmagdy@gmail.com>
2020-02-04 11:22:49 +0200

    updating sources

--- precache-manifest.e3f414e13bfab8d55766d115ecfba161.js
+++ precache-manifest.e3f414e13bfab8d55766d115ecfba161.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "be52258b9d544597527f",
-    "url": "/css/app.9ea745cc.css"
+    "url": "./css/app.9ea745cc.css"
   },
   {
     "revision": "49b2da1a7af1a41bdc42",
-    "url": "/css/chunk-vendors.7caec713.css"
+    "url": "./css/chunk-vendors.7caec713.css"
   },
   {
     "revision": "d856a793869c68a7065713881e251571",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "be52258b9d544597527f",
-    "url": "/js/app.4009aa9e.js"
+    "url": "./js/app.4009aa9e.js"
   },
   {
     "revision": "49b2da1a7af1a41bdc42",
-    "url": "/js/chunk-vendors.2f9d08ae.js"
+    "url": "./js/chunk-vendors.2f9d08ae.js"
   },
   {
     "revision": "aebdec95904dfac0846010b7e242b8c2",
-    "url": "/manifest.json"
+    "url": "./manifest.json"
   },
   {
     "revision": "4067a61c7ed086a837b2974b2363bed7",
-    "url": "/point-blank.mp3"
+    "url": "./point-blank.mp3"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "./robots.txt"
   }
 ]);
