55ddd2eeb4f22870bd6b9c57c46200c299765cbc
ZED-Magdy <zedmagdy@gmail.com>
2020-02-04 11:20:18 +0200

    updating sources

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3f414e13bfab8d55766d115ecfba161.js"
+  "./precache-manifest.e3f414e13bfab8d55766d115ecfba161.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "todo"});
